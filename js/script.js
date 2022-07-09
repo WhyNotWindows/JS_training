02ac878469bbf4f4b0c3056b26becbb3eadcffba
Yaroslav <yaroslavgajdash@gmail.com>
2022-07-09 16:56:14 +0200

    added loop

--- js/script.js
+++ js/script.js
@@ -8,12 +8,36 @@ const personalMovieDB = {
     privat: false
 };
 
-const movieName1 = prompt('Один из последних просмотренных фильмов?', '');
+/* const movieName1 = prompt('Один из последних просмотренных фильмов?', '');
 const movieRate1 = prompt('На сколько бы вы его оценили?', '');
 const movieName2 = prompt('Один из последних просмотренных фильмов?', '');
 const movieRate2 = prompt('На сколько бы вы его оценили?', '');
 
 personalMovieDB.movies[movieName1] = movieRate1;
-personalMovieDB.movies[movieName2] = movieRate2;
+personalMovieDB.movies[movieName2] = movieRate2; */
+
+for (let i = 0; i < 2; i++) {
+    let movieName = prompt('Один из последних просмотренных фильмов?', '');
+    if (!movieName || movieName.length > 50) {
+        i -= 1;
+        continue;
+    }
+    let movieRate = prompt('На сколько бы вы его оценили?', '');
+    if (!movieRate) {
+        i -= 1;
+        continue;
+    }
+    personalMovieDB.movies[movieName] = movieRate;
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
--- js/testing.js
+++ js/testing.js
@@ -1,16 +1,77 @@
-let l1 = [3, 6, 8, 0];
-let l2 = [3, 6, 8, 0];
-let num1 = 0;
-let num2 = 0;
-for(let i = 0; i < l1.length; i++) {
-    num1 += l1[i] * 10**(l1.length - i - 1);
+let s = ["M", "C", "M", "X", "C", "I", "V"];
+
+let result = 0;
+    
+let I = 1;
+let V = 5;
+let X = 10;
+let L = 50;
+let C = 100;
+let D = 500;
+let M = 1000;
+
+for (let i = 0; i <= s.length; i++) {
+    //1-10
+    if (s[i] == 'I' && s[i+1] != 'V' && s[i+1] != 'X') {
+        result += I;
+        console.log ("+1");
+    } else if (s[i] == 'I' && s[i+1] == 'V') {
+        result += 4;
+        console.log ("+4");
+        i += 1;
+        continue;
+    } else if (s[i] == 'I' && s[i+1] == 'X') {
+        result += 9;
+        console.log ("+9");
+        i += 1;
+        continue;
+    }
+    if (s[i] == 'V') {
+        result += V;
+        console.log ("+5");
+    }
+    //10-100
+    if (s[i] == 'X' && s[i+1] != 'L' && s[i+1] != 'C') {
+        result += X;
+        console.log ("+10");
+    } else if (s[i] == 'X' && s[i+1] == 'L') {
+        result += 40;
+        console.log ("+40");
+        i += 1;
+        continue;
+    } else if (s[i] == 'X' && s[i+1] == 'C') {
+        result += 90;
+        console.log ("+90");
+        i += 1;
+        continue;
+    }
+    if (s[i] == 'L') {
+        result += L;
+        console.log ("+50");
+    }
+    //100-1000
+    if (s[i] == 'C' && s[i+1] != 'D' && s[i+1] != 'M') {
+        result += C;
+        console.log ("+100");
+    } else if (s[i] == 'C' && s[i+1] == 'D') {
+        result += 400;
+        console.log ("+400");
+        i += 1;
+        continue;
+    } else if (s[i] == 'C' && s[i+1] == 'M') {
+        result += 900;
+        console.log ("+900");
+        i += 1;
+        continue;
+    }
+    if (s[i] == 'D') {
+        result += D;
+        console.log ("+500");
+    }
+    if (s[i] == 'M') {
+        result += M;
+        console.log ("+1000");
+    }
 }
-for(let i = 0; i < l2.length; i++) {
-    num2 += l1[i] * 10**(l2.length - i - 1);
-}
-let str = String(num1 + num2);
-let result = [];
-for (let i = 0; i < str.length; i++) {
-    result[i] = Number(str[str.length - i - 1]);
-}
-console.log(result);
+
+console.log (result);
